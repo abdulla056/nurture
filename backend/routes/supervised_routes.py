5b1784a71f578549026ba8c5c0f7d644ac194529
zhehinnn <156086339+zhehinnn@users.noreply.github.com>
2025-02-17 16:32:57 +0800

    Merge pull request #11 from abdulla056/firebase
    Firebase

--- backend/routes/supervised_routes.py
+++ backend/routes/supervised_routes.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify
-import numpy as np
-import joblib
+from flask import Flask, request, jsonify, Blueprint # type: ignore
+import numpy as np # type: ignore
+import joblib # type: ignore
 import os
 from flask_cors import CORS  # Allow cross-origin requests
+import firebase_admin # type: ignore
+from firebase_admin import credentials,firestore # type: ignore
+from config import Config
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS
+firebase_config_json = Config.FIREBASE_CONFIG
+cred = credentials.Certificate(firebase_config_json)
+
+db = firestore.client()
+supervised_bp = Blueprint('supervised_bp', __name__)
 
 # Load trained model and scaler
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "models", "best_DEMOoverall_model.pkl")
@@ -14,7 +20,7 @@ model = joblib.load(MODEL_PATH)
 SCALER_PATH = os.path.join(os.path.dirname(__file__), "models", "DEMOscaler.pkl")
 scaler = joblib.load(SCALER_PATH)
 
-@app.route("/predict", methods=["POST"])
+@supervised_bp.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.get_json()
@@ -38,6 +44,3 @@ def predict():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500  # Return error response with status code 500
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
