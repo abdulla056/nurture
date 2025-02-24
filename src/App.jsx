0d2460fcb6a995704339b59bf7ab7651984f3683
zhehinnn <156086339+zhehinnn@users.noreply.github.com>
2025-02-24 15:21:27 +0800

    Added a function to attach authorisation header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 function App() {
+  // useEffect(() => {
+  //   const token = localStorage.getItem('token'); 
+  //   if (token) {
+  //     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  //   } else{
+  //     navigate("/login");
+  //   }
+  // }, []); 
   return (
     <>
       <AppRoutes />
