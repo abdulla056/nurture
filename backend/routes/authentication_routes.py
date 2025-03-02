d0a466bd8e7a4b701dd88811a24327567001a3ba
zhehinnn <156086339+zhehinnn@users.noreply.github.com>
2025-03-03 01:12:56 +0800

    No more printing tokens out

--- backend/routes/authentication_routes.py
+++ backend/routes/authentication_routes.py
@@ -96,8 +96,6 @@ def verify_token():
         if not id_token:
             return jsonify({'error': 'Token is required'}), 400
 
-        # Verify the Firebase ID token
-        print('Received',id_token)
 
         decoded_token = auth.verify_id_token(id_token)
         print("decoded")
