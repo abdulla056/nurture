588375ab06d4fc3dbd652cd525a68f11ca040fa8
zhehinnn <156086339+zhehinnn@users.noreply.github.com>
2025-03-12 17:49:53 +0800

    PublicRoute has been used to protect the login and register routes. The ProtectedRoute component has been updated to redirect to the selection dashboard page if the user is authenticated. The App component has been updated to use the PublicRoute and ProtectedRoute components. The routes.jsx file has been updated to use the PublicRoute and ProtectedRoute components.

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -2,31 +2,12 @@ import { Navigate } from "react-router-dom";
 import api from "../../services/api";
 import { useContext, useState } from "react";
 import { UserDetailsContext } from "../../store/user-details-context";
+
 const ProtectedRoute = ({ element }) => {
-  const { isAuthenticated, setIsAuthenticated} = useContext(UserDetailsContext);
+  const { isAuthenticated, setIsAuthenticated, checkAuthCookie} = useContext(UserDetailsContext);
   // const [isAuthenticated, setIsAuthenticated] = useState(null);
-    const checkAuthCookie = async () => {
-      try {
-        const response = await api.get("/auth/check_cookie", { withCredentials: true });
-        if (response.data[1].valid) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (error) {
-        if (error.response && error.response.status === 404) {
-          setIsAuthenticated(false);
-        } else {
-          console.error("Error checking authentication:", error);
-          setIsAuthenticated(false);
-        }
-      }
-    };
-    checkAuthCookie();
     
-    if (isAuthenticated === null) {
-      return null; // or a loading spinner
-    }
+    checkAuthCookie();
   
   return isAuthenticated ? element : <Navigate to="/authentication" />;
 };
