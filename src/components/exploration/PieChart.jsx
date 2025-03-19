1acdf8ea6df684362a2b13decba2993def48862d
abdulla056 <115686255+abdulla056@users.noreply.github.com>
2025-03-19 17:11:57 +0800

    ok

--- src/components/exploration/PieChart.jsx
+++ src/components/exploration/PieChart.jsx
@@ -1,19 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import PrimaryContainer from "../layout/PrimaryContainer";
 import { PredictionDetailsContext } from "../../store/prediction-details-context";
+import { AnimatePresence, motion } from "framer-motion";
+import LoadingSpinner from "../common/LoadingSpinner";
+import PrimaryButton from "../common/PrimaryButton";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PieChart() {
   const { contributingFactors } = useContext(PredictionDetailsContext);
+  if (!contributingFactors) {
+    return ( 
+      <div style={{ paddingTop: '50px' }}> {/* Adjust the value as needed */}
+        <LoadingSpinner />
+      </div> // Show a loading spinner or message
+      )
+  }
   const contributingFactorsArray = Object.entries(contributingFactors).map(
     ([key, value]) => ({ key, value })
   );
   contributingFactorsArray.sort((a, b) => b.value - a.value);
   const labels = contributingFactorsArray.map((factor) => factor.key);
   const dataValues = contributingFactorsArray.map((factor) => factor.value);
+  const chartRef = useRef();
   // Data for the chart
   const data = {
     labels: labels,
@@ -42,12 +53,59 @@ export default function PieChart() {
     ],
   };
 
+
+  const downloadChart = () => {
+    if (chartRef.current) {
+      const chartInstance = chartRef.current;
+      const chartCanvas = chartInstance.canvas;
+      const image = chartCanvas.toDataURL("image/png");
+
+      // Create a download link
+      const link = document.createElement("a");
+      link.href = image;
+      link.download = "pie_chart.png";
+      link.click();
+    }
+  };
+
   return (
     <PrimaryContainer className="pt-12 w-3/5">
       <h2 className="font-medium text-center mb-4">Pie Chart</h2>
       <div className="border rounded-lg border-[#C5C5C5] w-full">
-        <Pie data={data} className="p-6" />
+        <Pie data={data} className="p-6" ref={chartRef} />
       </div>
+      <div
+          className="gap-4 flex flex-row fixed left-1/2  transform -translate-x-1/2 -translate-y-1/2"
+          style={{ top: "87%" }}
+        >
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: "easeOut", delay: 0.5 }}
+          >
+            <PrimaryButton
+              className={
+                "opacity-75 hover:opacity-100 bg-primary text-white py-6 px-10 rounded-2xl text-xl hover:scale-105"
+              }
+            >
+              Export to CSV
+            </PrimaryButton>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: "easeOut", delay: 0.5 }}
+          >
+            <PrimaryButton
+              className={
+                "opacity-75 hover:opacity-100 bg-primary text-white py-6 px-10 rounded-2xl text-xl hover:scale-105"
+              }
+              onClick={downloadChart}
+            >
+              Download PDF
+            </PrimaryButton>
+          </motion.div>
+        </div>
     </PrimaryContainer>
   );
 }
--- src/pages/DataExploration.jsx
+++ src/pages/DataExploration.jsx
@@ -14,37 +14,6 @@ export default function DataExploration() {
       >
         <ExplorationNavBar />
         <Outlet />
-        <div
-          className="gap-4 flex flex-row fixed left-1/2  transform -translate-x-1/2 -translate-y-1/2"
-          style={{ top: "87%" }}
-        >
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut", delay: 0.5 }}
-          >
-            <PrimaryButton
-              className={
-                "opacity-75 hover:opacity-100 bg-primary text-white py-6 px-10 rounded-2xl text-xl hover:scale-105"
-              }
-            >
-              Export to CSV
-            </PrimaryButton>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut", delay: 0.5 }}
-          >
-            <PrimaryButton
-              className={
-                "opacity-75 hover:opacity-100 bg-primary text-white py-6 px-10 rounded-2xl text-xl hover:scale-105"
-              }
-            >
-              Download PDF
-            </PrimaryButton>
-          </motion.div>
-        </div>
       </motion.div>
     </AnimatePresence>
   );
